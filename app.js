6341ab678445e78d20561d21d477b5f0ac0af638
Marita Sailor <maritasailor@ho.fios-router.home>
2017-12-09 01:19:27 -0500

    added app.listen line

--- app.js
+++ app.js
@@ -48,4 +48,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server listening");
+});
+
+
 module.exports = app;
