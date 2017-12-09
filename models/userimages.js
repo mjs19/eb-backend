ee3689913deaa86a8b565d82dd66c2e25ab9cbb6
Marita Sailor <maritasailor@ho.fios-router.home>
2017-12-09 15:36:24 -0500

    plz work

--- models/userimages.js
+++ models/userimages.js
@@ -3,12 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   var userImages = sequelize.define('userImages', {
     userId: DataTypes.INTEGER,
     imageId: DataTypes.INTEGER
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
+  }
+  
   return userImages;
 };
