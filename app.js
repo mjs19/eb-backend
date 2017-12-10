1826953775da4390a9e6d66c44703f274cc77d0a
Marita Sailor <maritasailor@ho.fios-router.home>
2017-12-10 17:18:29 -0500

    added login

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-// var login = require('./routes/login');
+var login = require('./routes/login');
 var images = require('./routes/images');
 var gifs = require('./routes/gifs');
 var app = express();
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-// app.use('/login', login);
+app.use('/login', login);
 app.use('/images', images);
 app.use('/gifs', gifs);
 
