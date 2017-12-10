ad57e515eed0a05c94513841d423b4e78d723c83
Marita Sailor <maritasailor@ho.fios-router.home>
2017-12-10 17:12:21 -0500

    removed tings from images

--- routes/images.js
+++ routes/images.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../models');
-var request = require('request');
-var indico = require('indico.io');
-indico.apiKey =  '7a00385289015143942a0b624681cd19';
-const clientId = '8945a218b217c4f';
+// var request = require('request');
+// var indico = require('indico.io');
+// indico.apiKey =  '7a00385289015143942a0b624681cd19';
+// const clientId = '8945a218b217c4f';
 // const clientSecret = '1f67214be6fde71891ee7ce43c89ee7f55fd411a';
 
 /* GET all images */
