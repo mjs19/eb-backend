66ea90cf5b00f8e3fe9430f444e464b738641408
Marita Sailor <maritasailor@ho.local>
2017-12-11 16:33:25 -0500

    removed imagur

--- routes/images.js
+++ routes/images.js
@@ -3,14 +3,8 @@ var router = express.Router();
 const db = require('../models');
 var request = require('request');
 var indico = require('indico.io');
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 indico.apiKey =  '7a00385289015143942a0b624681cd19';
 const clientId = '8945a218b217c4f';
-const cloudName = 'emotionalbreakdown';
-const unsignedUploadPreset = 'atg4ausd';
-var FormData = require('form-data');
-var FileAPI = require('file-api')
-var File = FileAPI.File;
 
 var rp = require('request-promise');
 
@@ -41,48 +35,32 @@ router.get('/*', (req, res) => {
 // /* POST image */
 router.post('/', (req, res) => {
   // post file to imgur to generate url
-  request({
-      url: 'https://api.imgur.com/3/image',
-      method: 'POST',
-      json: true,
-      headers: {
-          'authorization': `Client-ID ${clientId}`,
-          'content-type': 'application/json'
-      },
-      body: { image: req.body.image } // image must be a binary file, base64 data, or a URL
-  }, function(error, response, body){
-      if(error) {
-          res.json(error);
-      } else {
-          var imageUrl = response.body.data.link;
-          var response = function(res) { console.log(res); }
-          var logError = function(err) { console.log(err); }
-          // single example
-          indico.fer(imageUrl)
-          .then(response => {
-              db.image.create({
-                  url: imageUrl,
-                  fave: false,
-                  neutral: response.Neutral,
-                  happy: response.Happy,
-                  sad: response.Sad,
-                  surprised: response.Surprise,
-                  fearful: response.Fear,
-                  angry: response.Angry,
-                  createdAt: new Date(),
-                  updatedAt: new Date()
-              })
-              .then(newImage => {
-                  res.json({
-                      'message': 'image created!!',
-                      'image': newImage
-                  });
-              });
-          })
-          .catch(logError);
-      }
+  var imageUrl = response.body.imageUrl;
+  var response = function(res) { console.log(res); }
+  var logError = function(err) { console.log(err); }
+  // single example
+  indico.fer(imageUrl)
+  .then(response => {
+    db.image.create({
+      url: imageUrl,
+      fave: false,
+      neutral: response.Neutral,
+      happy: response.Happy,
+      sad: response.Sad,
+      surprised: response.Surprise,
+      fearful: response.Fear,
+      angry: response.Angry,
+      createdAt: new Date(),
+      updatedAt: new Date()
     })
-
+    .then(newImage => {
+      res.json({
+        'message': 'image created!!',
+        'image': newImage
+      });
+    });
+  })
+  .catch(logError);
 
 });
 
