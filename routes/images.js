e0d41024d9e71662fb2b1eb2b833a96d89d8ad7b
Marita Sailor <maritasailor@ho.local>
2017-12-11 14:49:46 -0500

    removed event listener

--- routes/images.js
+++ routes/images.js
@@ -86,14 +86,6 @@ router.post('/', (req, res) => {
   var fd = new FormData();
   xhr.open('POST', url, true);
   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  // Update progress (can be used to show progress indicator)
-   xhr.upload.addEventListener("progress", function(e) {
-     var progress = Math.round((e.loaded * 100.0) / e.total);
-     document.getElementById('progress').style.width = progress + "%";
-
-     console.log(`fileuploadprogress data.loaded: ${e.loaded},
-   data.total: ${e.total}`);
-   });
 
   xhr.onreadystatechange = function(e) {
   if (xhr.readyState == 4 && xhr.status == 200) {
