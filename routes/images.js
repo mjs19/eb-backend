1414e9bae12ac0be7267ff365c1495285501b7d7
Marita Sailor <maritasailor@ho.fios-router.home>
2017-12-12 00:36:37 -0500

    ?

--- routes/images.js
+++ routes/images.js
@@ -5,10 +5,6 @@ var request = require('request');
 var indico = require('indico.io');
 indico.apiKey =  '7a00385289015143942a0b624681cd19';
 const clientId = '8945a218b217c4f';
-<<<<<<< HEAD
-=======
-
->>>>>>> 0f1d49da97c7199eedb49b8bb9aae394d417a43b
 
 var rp = require('request-promise');
 
@@ -39,35 +35,33 @@ router.get('/*', (req, res) => {
 // /* POST image */
 router.post('/', (req, res) => {
   // post file to imgur to generate url
-  var imageUrl = req.body;
-  console.log("THIS IS IMAGE URL: ", req.body);
-  // var response = function(res) { console.log(res); }
-  // var logError = function(err) { console.log(err); }
+  var imageUrl = req.body.imageUrl;
+  console.log("THIS IS IMAGE URL: ", req.body.imageUrl);
+  var response = function(res) { console.log(res); }
+  var logError = function(err) { console.log(err); }
   // single example
-  // indico.fer(imageUrl)
-  // .then(response => {
-  //   db.image.create({
-  //     url: imageUrl,
-  //     fave: false,
-  //     neutral: response.Neutral,
-  //     happy: response.Happy,
-  //     sad: response.Sad,
-  //     surprised: response.Surprise,
-  //     fearful: response.Fear,
-  //     angry: response.Angry,
-  //     createdAt: new Date(),
-  //     updatedAt: new Date()
-  //   })
-  //   .then(newImage => {
-  //     res.json({
-  //       'message': 'image created!!',
-  //       'image': newImage
-  //     });
-  //   });
-  // })
-  // .catch(logError);
-
-
+  indico.fer(imageUrl)
+  .then(response => {
+    db.image.create({
+      url: imageUrl,
+      fave: false,
+      neutral: response.Neutral,
+      happy: response.Happy,
+      sad: response.Sad,
+      surprised: response.Surprise,
+      fearful: response.Fear,
+      angry: response.Angry,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+    .then(newImage => {
+      res.json({
+        'message': 'image created!!',
+        'image': newImage
+      });
+    });
+  })
+  .catch(logError);
 });
 
 // /* DELETE an image */
